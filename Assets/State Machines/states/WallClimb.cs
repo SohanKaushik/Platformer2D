03a0d6f54866c85124d0303f8570b3a6df5bfae2
SohanKaushik <sohangamedev@gmail.com>
2025-08-04 20:16:33 +0530

    Physics: added wall jumping

--- Assets/State Machines/states/WallClimb.cs
+++ Assets/State Machines/states/WallClimb.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class WallClimbState : PlayerState
 {
     private float wallCrawlSpeed = 5.0f;
+    private Vector2 wallJumpForce = new Vector2(30, 40);
+
+    private bool _wallJumped;
 
     public WallClimbState(Player player, PlayerStateMachine state, PlayerStateList name) : base(player, state, name)
     {
@@ -21,12 +24,21 @@ public class WallClimbState : PlayerState
 
         // # jump
         if (player._context.jumpRequest) {
-            
+            _wallJumped = true;
         }
     }
 
     public override void FixedUpdate()
     {
+        if (_wallJumped) { 
+            player._velocity = wallJumpForce;
+            return;
+        }
        player._velocity.y = player.GetAxisDirections().y * wallCrawlSpeed;
+    }
+
+    public override void OnExit()
+    {
+        _wallJumped = false;
     }
 }
